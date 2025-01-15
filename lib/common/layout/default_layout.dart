78873a829e4d117760054158397ae699b93fdcc1
jungsun park <jungsun.park@kwatercraft.com>
2025-01-15 11:02:23 +0900

    bottom navigation 처리 추가

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -3,15 +3,43 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Color backgroundColor;
   final Widget child;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
-  const DefaultLayout(
-      {super.key, required this.child, this.backgroundColor = Colors.white});
+  const DefaultLayout({
+    super.key,
+    required this.child,
+    this.title,
+    this.bottomNavigationBar,
+    this.backgroundColor = Colors.white,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
+    );
+  }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    }
+    return AppBar(
+      backgroundColor: Colors.white,
+      automaticallyImplyLeading: false,
+      title: Text(
+        title!,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      elevation: 0,
+      centerTitle: true,
     );
   }
 }

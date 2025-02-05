867fa35daae5000e81f4f2ccd24626bdf79586bd
jungsun park <jungsun.park@kwatercraft.com>
2025-02-05 10:17:12 +0900

    listview 공통화 작업 추가

--- lib/common/common/pagination_list_view.dart
+++ lib/common/common/pagination_list_view.dart
@@ -1,14 +1,29 @@
+import 'package:actual/common/model/cursor_pagination_model.dart';
+import 'package:actual/common/model/model_with_id.dart';
+import 'package:actual/common/provider/pagination_provider.dart';
+import 'package:actual/common/utils/pagination_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PaginationListView extends ConsumerStatefulWidget {
-  const PaginationListView({super.key});
+typedef PaginationWidgetBuilder<T extends IModelWithId> = Widget Function(
+    BuildContext context, int index, T model);
+
+class PaginationListView<T extends IModelWithId>
+    extends ConsumerStatefulWidget {
+  final StateNotifierProvider<PaginationProvider, CursorPaginationBase>
+      provider;
+  final PaginationWidgetBuilder<T> itemBuilder;
+
+  const PaginationListView(
+      {super.key, required this.provider, required this.itemBuilder});
 
   @override
-  ConsumerState<PaginationListView> createState() => _PaginationListViewState();
+  ConsumerState<PaginationListView> createState() =>
+      _PaginationListViewState<T>();
 }
 
-class _PaginationListViewState extends ConsumerState<PaginationListView> {
+class _PaginationListViewState<T extends IModelWithId>
+    extends ConsumerState<PaginationListView> {
   final ScrollController _scrollController = ScrollController();
 
   @override
@@ -19,14 +34,79 @@ class _PaginationListViewState extends ConsumerState<PaginationListView> {
   }
 
   void listener() {
-    // PaginationUtils.paginate(
-    //   scrollController: _scrollController,
-    //   provider: ref.read(restaurantProvider.notifier),
-    // );
+    PaginationUtils.paginate(
+      scrollController: _scrollController,
+      provider: ref.read(widget.provider.notifier),
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(listener);
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final state = ref.watch(widget.provider);
+
+    if (state is CursorPaginationLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (state is CursorPaginationError) {
+      // return Center(
+      //   child: Text(state.message),
+      // );
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            state.message,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              ref.read(widget.provider.notifier).paginate(forceRefetch: true);
+            },
+            child: Text('Retry'),
+          ),
+        ],
+      );
+    }
+
+    final cp = state as CursorPagination<T>;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: ListView.separated(
+        controller: _scrollController,
+        itemCount: cp.data.length + 1,
+        itemBuilder: (_, index) {
+          if (index == cp.data.length) {
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Center(
+                child: cp is CursorPaginationFetchingMore
+                    ? CircularProgressIndicator()
+                    : Text(''),
+              ),
+            );
+          }
+
+          final parsedItem = cp.data[index];
+
+          return widget.itemBuilder(context, index, parsedItem);
+        },
+        separatorBuilder: (_, index) {
+          return const SizedBox(height: 16);
+        },
+      ),
+    );
   }
 }
--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,92 +1,28 @@
-import 'package:actual/common/model/cursor_pagination_model.dart';
-import 'package:actual/common/utils/pagination_utils.dart';
+import 'package:actual/common/common/pagination_list_view.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:actual/restaurant/provider/restaurant_provider.dart';
 import 'package:actual/restaurant/view/restaurant_detail_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RestaurantScreen extends ConsumerStatefulWidget {
-  const RestaurantScreen({super.key});
-
-  @override
-  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
-}
-
-class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
-  final ScrollController _scrollController = ScrollController();
-
-  @override
-  void initState() {
-    super.initState();
-
-    _scrollController.addListener(scrollListener);
-  }
-
-  void scrollListener() {
-    PaginationUtils.paginate(
-      scrollController: _scrollController,
-      provider: ref.read(restaurantProvider.notifier),
-    );
-  }
-
+class RestaurantScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final data = ref.watch(restaurantProvider);
-    if (data is CursorPaginationLoading) {
-      return const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-
-    if (data is CursorPaginationError) {
-      return Center(
-        child: Text(data.message),
-      );
-    }
-
-    final cp = data as CursorPagination;
-
-    return Container(
-      child: Center(
-        child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ListView.separated(
-              controller: _scrollController,
-              itemCount: cp.data.length + 1,
-              itemBuilder: (_, index) {
-                if (index == cp.data.length) {
-                  return Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                    child: Center(
-                      child: data is CursorPaginationFetchingMore
-                          ? CircularProgressIndicator()
-                          : Text(''),
-                    ),
-                  );
-                }
-
-                final parsedItem = cp.data[index];
-
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (_) => RestaurantDetailScreen(
-                          // id: '${parsedItem.id}1',
-                          id: parsedItem.id,
-                        ),
-                      ),
-                    );
-                  },
-                  child: RestaurantCard.fromModel(model: parsedItem),
-                );
-              },
-              separatorBuilder: (_, index) {
-                return const SizedBox(height: 16);
-              },
-            )),
-      ),
-    );
+    return PaginationListView(
+        provider: restaurantProvider,
+        itemBuilder: <RestaurantModel>(context, index, model) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => RestaurantDetailScreen(
+                    // id: '${parsedItem.id}1',
+                    id: model.id,
+                  ),
+                ),
+              );
+            },
+            child: RestaurantCard.fromModel(model: model),
+          );
+        });
   }
 }
